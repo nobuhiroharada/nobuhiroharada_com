2fbc201b0a2c805a935e10cb3f753360b144b319
nobuhiroharada <nobuhiroharada1026@gmail.com>
2020-05-11 07:52:17 +0900

    サイドバー追加

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,16 +3,19 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import Head from '../components/head'
+import aboutStyles from './about.module.scss'
 
 const AboutPage = () => {
-  return (
-    <Layout>
-      <Head title="About"/>
-      <h1>About</h1>
-      <p>工事中</p>
-      <p><Link to="/">TOPに戻る</Link></p>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<Head title="About"/>
+			<div className={aboutStyles.content}>
+				<h1>About</h1>
+				<p>工事中</p>
+				<p><Link to="/">TOPに戻る</Link></p>
+			</div>
+		</Layout>
+	)
 }
 
 export default AboutPage
